05b3ec3ba8366f875743fb95d343f21378e81e26
Dharshini-S12 <118833017+Dharshini-S12@users.noreply.github.com>
2024-02-01 21:23:35 +0530

    .

--- demo-file/src/components/Header.js
+++ demo-file/src/components/Header.js
@@ -39,7 +39,6 @@ function Header() {
               <span><img src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png'></img></span>
             </div>
         </div>
-
     </div>
   )
 }
