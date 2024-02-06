55f3bb4796b123ea19cc46e4c4f6b6827d0525f0
Dharshini-S12 <118833017+Dharshini-S12@users.noreply.github.com>
2024-02-06 21:45:27 +0530

    .

--- demo-file/src/App.js
+++ demo-file/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1 className="text-7xl text-center text-blue-400"> Hello world</h1>
+      <Header></Header>
     </div>
   );
 }
--- demo-file/src/components/Header.js
+++ demo-file/src/components/Header.js
@@ -1,21 +1,32 @@
 import React from 'react'
-
-function Header() {    
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+function Header() {  
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };  
   return (
     <div className="w-3/4 m-auto">
       <div className="mt-20">
+      <Slider {...settings}>
         {data.map((d) => (
-          <div className="bg-white h-[450px] text-black rounded-xl">
-            <div className="rounded-t-xl bg-indigo-500 flex justify-center items-center">
+          <div className="bg-white h-[400px] text-black rounded-xl">
+            <div className="h-56 rounded-t-xl bg-indigo-500 flex justify-center items-center">
               <img src={d.img} alt='' className="h-44 w-44 rounded-full"/>
             </div>
-            <div>
-               <p>{d.name}</p>
+            <div className='bg-white flex flex-col justify-center items-center gap-4 p-4'>
+               <p className='text-xl font-semibold'>{d.name}</p>
                <p>{d.review}</p>
-               <button>Read More</button>
+               <button className='bg-indigo-500 text-white text-lg px-6 rounded-xl'>Read More</button>
             </div>
           </div>
         ))}
+        </Slider>
       </div> 
         
     </div>
